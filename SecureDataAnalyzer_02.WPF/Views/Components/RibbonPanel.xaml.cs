94842a5902925e731312ec30b8b43edd790eeb0f
H_Shinohara <shinohara@softcreate.net>
2026-04-24 16:29:09 +0900

    グラフ表示チェックボックスを右端に移動

--- SecureDataAnalyzer_02.WPF/Views/Components/RibbonPanel.xaml.cs
+++ SecureDataAnalyzer_02.WPF/Views/Components/RibbonPanel.xaml.cs
@@ -13,7 +13,8 @@ namespace SecureDataAnalyzer_02.WPF.Views.Components
         private void CheckBox_Click(object sender, RoutedEventArgs e)
         {
             var chk = sender as CheckBox;
-            if (chk == null) return;
+            if (chk == null || chk.Tag == null) return;
+
             int graphNum = int.Parse(chk.Tag.ToString());
             bool isVisible = chk.IsChecked ?? true;
 
@@ -31,6 +32,19 @@ namespace SecureDataAnalyzer_02.WPF.Views.Components
             else if (graphNum == 4) Chk4.IsChecked = isChecked;
             else if (graphNum == 5) Chk5.IsChecked = isChecked;
             else if (graphNum == 6) Chk6.IsChecked = isChecked;
+        }
+
+        private void ShowGraphBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Window.GetWindow(this) as MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.GraphArea.Visibility = Visibility.Visible;
+                mainWindow.MySplitter.Visibility = Visibility.Visible;
+                mainWindow.GraphColumn.Width = new GridLength(1, GridUnitType.Star);
+                mainWindow.OpenGraphBtn.Visibility = Visibility.Collapsed;
+                this.ShowGraphBtn.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
