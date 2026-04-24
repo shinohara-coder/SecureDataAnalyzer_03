9d0b935f8119879e9f304ac376376f9e16421d12
H_Shinohara <shinohara@softcreate.net>
2026-04-24 11:30:15 +0900

    プロジェクトファイルを追加。

---
+++ SecureDataAnalyzer_02.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SecureDataAnalyzer_02.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
