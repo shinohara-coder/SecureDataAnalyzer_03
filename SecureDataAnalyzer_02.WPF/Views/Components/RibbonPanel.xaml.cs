92f3e2e0d1730072928a78e0b62e6d460ab53c08
H_Shinohara <shinohara@softcreate.net>
2026-04-24 14:23:00 +0900

    グラフ表示ボタンの配置変更

--- SecureDataAnalyzer_02.WPF/Views/Components/RibbonPanel.xaml.cs
+++ SecureDataAnalyzer_02.WPF/Views/Components/RibbonPanel.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace SecureDataAnalyzer_02.WPF.Views.Components
 {
@@ -9,25 +8,6 @@ namespace SecureDataAnalyzer_02.WPF.Views.Components
         {
             InitializeComponent();
         }
-
-        public void SetShowButtonVisibility(Visibility vis)
-        {
-            ShowGraphBtn.Visibility = vis;
-        }
-
-        private void ShowGraphBtn_Click(object sender, RoutedEventArgs e)
-        {
-            var mainWindow = Window.GetWindow(this) as MainWindow;
-            if (mainWindow != null)
-            {
-                mainWindow.GraphArea.Visibility = Visibility.Visible;
-                mainWindow.GraphColumn.Width = new GridLength(1, GridUnitType.Star);
-
-                // 【追加】境界線（スプリッター）を再表示する
-                mainWindow.MySplitter.Visibility = Visibility.Visible;
-
-                this.ShowGraphBtn.Visibility = Visibility.Collapsed;
-            }
-        }
+        // ShowGraphBtn 関連のコードはすべて削除しました
     }
 }
--- SecureDataAnalyzer_02.WPF/Views/MainWindow.xaml.cs
+++ SecureDataAnalyzer_02.WPF/Views/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ namespace SecureDataAnalyzer_02.WPF.Views
             InitializeComponent();
         }
 
+        // リボン格納
         private void ToggleRibbon_Click(object sender, RoutedEventArgs e)
         {
             if (MyRibbon.Visibility == Visibility.Visible)
@@ -23,15 +24,28 @@ namespace SecureDataAnalyzer_02.WPF.Views
             }
         }
 
+        // グラフエリアを閉じる（×ボタン）
         private void CloseGraph_Click(object sender, RoutedEventArgs e)
         {
             GraphArea.Visibility = Visibility.Collapsed;
+            MySplitter.Visibility = Visibility.Collapsed;
+            // 右側の列を完全に消す
             GraphColumn.Width = new GridLength(0);
 
-            // 【追加】境界線（スプリッター）も隠す
-            MySplitter.Visibility = Visibility.Collapsed;
+            // 再表示ボタンを出す
+            OpenGraphBtn.Visibility = Visibility.Visible;
+        }
 
-            MyRibbon.SetShowButtonVisibility(Visibility.Visible);
+        // グラフエリアを再表示する
+        private void OpenGraph_Click(object sender, RoutedEventArgs e)
+        {
+            GraphArea.Visibility = Visibility.Visible;
+            MySplitter.Visibility = Visibility.Visible;
+            // 右側の列を元の比率に戻す
+            GraphColumn.Width = new GridLength(1, GridUnitType.Star);
+
+            // 自分（再表示ボタン）を隠す
+            OpenGraphBtn.Visibility = Visibility.Collapsed;
         }
     }
 }
