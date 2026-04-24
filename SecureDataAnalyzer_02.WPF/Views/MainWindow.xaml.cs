44f9d31a8184941ded2c2e4d3ee063895683bf2b
H_Shinohara <shinohara@softcreate.net>
2026-04-24 13:33:53 +0900

    メイン画面の枠組み実装

--- SecureDataAnalyzer_02.WPF/Views/MainWindow.xaml.cs
+++ SecureDataAnalyzer_02.WPF/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SecureDataAnalyzer_02.WPF
+namespace SecureDataAnalyzer_02.WPF.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
