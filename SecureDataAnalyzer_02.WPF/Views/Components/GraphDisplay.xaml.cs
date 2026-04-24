27fc65e280c0380e24ad8e1be3c024084f882eca
H_Shinohara <shinohara@softcreate.net>
2026-04-24 15:08:34 +0900

    グラフの表示・非表示方法を変更

--- SecureDataAnalyzer_02.WPF/Views/Components/GraphDisplay.xaml.cs
+++ SecureDataAnalyzer_02.WPF/Views/Components/GraphDisplay.xaml.cs
@@ -1,26 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SecureDataAnalyzer_02.WPF.Views.Components
 {
-    /// <summary>
-    /// GraphDisplay.xaml の相互作用ロジック
-    /// </summary>
     public partial class GraphDisplay : UserControl
     {
         public GraphDisplay()
         {
             InitializeComponent();
+        }
+
+        private void HideGraph_Click(object sender, RoutedEventArgs e)
+        {
+            // ボタンの「Tag」に書いてある名前を頼りに、どのグラフを隠すか判断します
+            var btn = sender as Button;
+            string targetName = btn.Tag.ToString();
+
+            if (targetName == "Graph2") Graph2.Visibility = Visibility.Collapsed;
+            if (targetName == "Graph3") Graph3.Visibility = Visibility.Collapsed;
+            if (targetName == "Graph4") Graph4.Visibility = Visibility.Collapsed;
+            if (targetName == "Graph5") Graph5.Visibility = Visibility.Collapsed;
+            if (targetName == "Graph6") Graph6.Visibility = Visibility.Collapsed;
+        }
+
+        // 外側（リボンなど）から表示状態を変えるためのメソッド
+        public void SetGraphVisibility(int number, bool isVisible)
+        {
+            var visibility = isVisible ? Visibility.Visible : Visibility.Collapsed;
+            switch (number)
+            {
+                case 2: Graph2.Visibility = visibility; break;
+                case 3: Graph3.Visibility = visibility; break;
+                case 4: Graph4.Visibility = visibility; break;
+                case 5: Graph5.Visibility = visibility; break;
+                case 6: Graph6.Visibility = visibility; break;
+            }
         }
     }
 }
--- SecureDataAnalyzer_02.WPF/Views/Components/RibbonPanel.xaml.cs
+++ SecureDataAnalyzer_02.WPF/Views/Components/RibbonPanel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace SecureDataAnalyzer_02.WPF.Views.Components
 {
@@ -8,6 +9,25 @@ namespace SecureDataAnalyzer_02.WPF.Views.Components
         {
             InitializeComponent();
         }
-        // ShowGraphBtn 関連のコードはすべて削除しました
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var chk = sender as CheckBox;
+            int graphNum = int.Parse(chk.Tag.ToString());
+            bool isVisible = chk.IsChecked ?? true;
+
+            // MainWindowを通じてGraphDisplayを操作する
+            var mainWindow = Window.GetWindow(this) as MainWindow;
+            if (mainWindow != null)
+            {
+                // GraphDisplayの中身を直接操作します
+                // MainWindow.xamlでGraphDisplayに x:Name="MyGraphContent" と付ける必要があります（後述）
+                mainWindow.MyGraphContent.SetGraphVisibility(graphNum, isVisible);
+            }
+        }
+
+        // 以前のメソッド
+        public void SetShowButtonVisibility(Visibility vis) { /* 以前と同じ */ }
+        private void ShowGraphBtn_Click(object sender, RoutedEventArgs e) { /* 以前と同じ */ }
     }
 }
